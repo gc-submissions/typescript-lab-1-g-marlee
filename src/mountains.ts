d98c5f2190cc7d23b38055f02a63cbf33494ceb3
Marlee Garcia <marlee.garcia121@gmail.com>
2022-06-22 21:05:03 -0400

    Creatted Mountain interface and mountains array

---
+++ src/mountains.ts
@@ -0,0 +1,18 @@
+interface Mountain {
+    name: string;
+    height: number;
+}
+
+const mountains: Mountain[] = [{
+    name: "Kilimanjaro",
+    height: 19341,
+    },
+    {
+    name: "Everest",
+    height: 29029,
+    },
+    {
+    name: "Denali",
+    height: 20310,
+    }
+    ];

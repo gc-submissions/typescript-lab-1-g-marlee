e3d492deeb1c80c47851a6f96683e4e2b9cf0fd9
Marlee Garcia <marlee.garcia121@gmail.com>
2022-06-22 21:43:38 -0400

    Imported Product and created InventoryItem and inventory

---
+++ src/inventory.ts
@@ -0,0 +1,18 @@
+import { Product } from "./products";
+
+interface InventoryItem {
+    product: Product;
+    quantity: number;
+};
+
+const inventory: InventoryItem[] = [{
+    product: {name: "motor", price: 10.00},
+    quantity: 10,
+    },
+    {
+    product: {name: "sensor", price: 12.50},
+    quantity: 4,
+    },{
+    product: {name: "LED", price: 1.00},
+    quantity: 20,
+}];

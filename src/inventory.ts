59b9744aec43ec30c11ba9a39e0f6daf79851058
Marlee Garcia <marlee.garcia121@gmail.com>
2022-06-22 21:48:39 -0400

    Wrote and export calcInventoryValue function

--- src/inventory.ts
+++ src/inventory.ts
@@ -16,3 +16,13 @@ const inventory: InventoryItem[] = [{
     product: {name: "LED", price: 1.00},
     quantity: 20,
 }];
+
+export function calcInventoryValue(inventory: InventoryItem[]): number {
+    let total = 0;
+
+    inventory.forEach(item => {
+        total += item.product.price * item.quantity;
+    });
+
+    return total;
+}

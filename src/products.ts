35c64b0ff21be38ec0d584444fcc1250dc37d7aa
Marlee Garcia <marlee.garcia121@gmail.com>
2022-06-22 21:30:54 -0400

    Create Product interface and products array

---
+++ src/products.ts
@@ -0,0 +1,15 @@
+interface Product {
+    name: string;
+    price: number;
+}
+
+const products: Product[] = [{
+    name: "paint",
+    price: 45,  
+    }, {
+    name: "brush",
+    price: 8,
+    }, {
+    name: "bucket",
+    price: 2,
+}];

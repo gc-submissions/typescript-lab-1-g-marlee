638f662310e6f3c925394602dfe3933d1c513e61
Marlee Garcia <marlee.garcia121@gmail.com>
2022-06-22 21:12:58 -0400

    Wrote findNameOfTallestMountain function

--- src/mountains.ts
+++ src/mountains.ts
@@ -1,4 +1,4 @@
-interface Mountain {
+export interface Mountain {
     name: string;
     height: number;
 }
@@ -15,4 +15,20 @@ const mountains: Mountain[] = [{
     name: "Denali",
     height: 20310,
     }
-    ];
+];
+
+export function findNameOfTallestMountain(mountains: Mountain[]): string {
+    let tallestHeight = 0;
+    let tallestName = "";
+    mountains.forEach(mountain => {
+        if (mountain.height > tallestHeight) {
+            tallestHeight = mountain.height;
+            tallestName = mountain.name;
+        }
+    });
+    return tallestName;
+};
+
+const tallestMountain = findNameOfTallestMountain(mountains);
+
+console.log(tallestMountain);
